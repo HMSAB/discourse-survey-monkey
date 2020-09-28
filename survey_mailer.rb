3eea0b65d77bd8052a39b57677af6b8003fc2343
TomKimsey <thk@hms.se>
2020-09-28 14:13:42 -0400

    Included BuildEmailHelper module in SurveyMailer class

--- survey_mailer.rb
+++ survey_mailer.rb
@@ -2,6 +2,7 @@ require 'email'
 require 'email/build_email_helper'
 
 class SurveyMailer < ActionMailer::Base
+  include Email::BuildEmailHelper
 
   def send_email(template, to_address, survey)
     build_email(
